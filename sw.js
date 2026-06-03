0f5a23ea4213bc99a71a703dbce3e9346024aed5
abdulrahman198 <96415042+abdulrahman198@users.noreply.github.com>
2026-06-03 17:45:50 +0200

    bump -> fl-v25

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // FALASTEEN.INK Service Worker v12
-var CACHE = 'fl-v24';
+var CACHE = 'fl-v25';
 var APP_SHELL = [
   '/', 'Index.html', 'Feed.html', 'Martyrs.html', 'Landmarks.html',
   'Map.html', 'Archive.html', 'live.html', 'Guardian_hub1.html',
