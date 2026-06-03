eb5fc5aa60d1a968bfcd506986fa5f1e37aaf971
abdulrahman198 <96415042+abdulrahman198@users.noreply.github.com>
2026-06-03 04:57:38 +0200

    chore: bump service worker cache to fl-v17

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // FALASTEEN.INK Service Worker v12
-var CACHE = 'fl-v16';
+var CACHE = 'fl-v17';
 var APP_SHELL = [
   '/', 'Index.html', 'Feed.html', 'Martyrs.html', 'Landmarks.html',
   'Map.html', 'Archive.html', 'live.html', 'Guardian_hub1.html',
